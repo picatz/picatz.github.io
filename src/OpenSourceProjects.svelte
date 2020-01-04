73c97cc85347b5169547276f0fded8bf084a38d6
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-01-04 15:32:07 -0500

    Remove gist links

--- src/OpenSourceProjects.svelte
+++ src/OpenSourceProjects.svelte
@@ -27,38 +27,6 @@
           <code style="background: black; color: greenyellow;">
             <span style="color:white">$</span> gem install shodanz
           </code>
-          <br>
-          <br>
-          <script src="https://gist.github.com/picatz/45e4d8b35a314abd244453d9cc712b65.js"></script>
-        </div>
-      </div>
-      <br>
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">
-          SubFinder v2
-          </p>
-          <div class="card-header-icon pull-right">
-            <a href="https://github.com/subfinder/research" class="button">
-              <span class="icon">
-                <i class="fab fa-github"></i>
-              </span>
-              <span>Source Code</span>
-            </a>
-          </div>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            I've been working on the next version of <a href="https://github.com/subfinder">SubFinder</a>, a subdomain enumeration application written in Go.
-          </div>
-          <strong>Install</strong>
-          <br>
-          <code style="background: black; color: greenyellow;">
-            <span style="color:white">$</span> go get github.com/subfinder/core/subzero
-          </code>
-          <br>
-          <br>
-          <script src="https://gist.github.com/picatz/82d286bcf5e0cb0956feecb6b1ca6898.js"></script>
         </div>
       </div>
       <br>
@@ -114,41 +82,5 @@
         </div>
       </div>
       <br>
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">
-            builderJS
-          </p>
-          <div class="card-header-icon pull-right">
-            <a href="https://codepen.io/anon/pen/Wgogvq" class="button">
-              <span class="icon">
-                <i class="fab fa-codepen"></i>
-              </span>
-              <span>Live Example</span>
-            </a>
-          </div>
-          <div class="card-header-icon pull-right">
-            <a href="https://github.com/picatz/builderJS" class="button">
-              <span class="icon">
-                <i class="fab fa-github"></i>
-              </span>
-              <span>Source Code</span>
-            </a>
-          </div>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            Simple, composable user interface builder.
-          </div>
-          <strong>Install</strong>
-          <br>
-          <code style="background: black; color: greenyellow;">
-            <span style="color:white">$</span> git clone https://github.com/picatz/builderJS
-          </code>
-          <br>
-          <br>
-          <script src="https://gist.github.com/picatz/670c56e6b995266ace8db3bec5db43e0.js"></script>
-        </div>
-      </div>
     </div>
 </section>
