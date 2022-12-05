77f9c6157a2a616341667d98dc37ea7e987849ec
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2022-12-05 06:07:01 +0000

    Update

---
+++ src/BlogPost.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let title = '';
+    export let id = '';
+    export let date = '';
+
+    function formatDate(date) {
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return new Date(date).toLocaleDateString([], options);
+    }
+</script>
+
+<div class="card">
+    <a href="/#blog/{id}">
+        <header class="card-header">
+            <p class="card-header-title">
+                <span class="tag" style="margin-right: 1rem">
+                    {formatDate(date)}
+                  </span>
+                {title}
+            </p>
+            <p>
+            </p>
+            <div class="card-header-icon pull-right">
+                <a href="/#blog/{id}" class="button">
+                    <span class="icon">
+                        <i class="fas fa-book"></i>
+                    </span>
+                </a>
+            </div>
+        </header>
+    </a>
+</div>
+
+<br>
---
+++ src/BlogPosts.svelte
@@ -0,0 +1,20 @@
+<script>
+    import BlogPost from './BlogPost.svelte';
+
+    export let posts = [];
+</script>
+  
+<section class="section">
+    <div class="container is-fluid">
+      <div class="content">
+        <h1>Blog Posts</h1>
+      </div>
+        {#each Object.entries(posts) as [id, post]}
+            <BlogPost
+            id={id}
+            title={post.title}
+            date={post.date}
+            />
+        {/each}
+    </div>
+</section>
