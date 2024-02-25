1f478568b447020bfead7051043f8b58faa3faac
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2024-02-25 14:46:24 -0500

    Start from scratch on `main`

--- src/Footer.svelte
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-  <div class="content has-text-centered if-flex is-flex-align-items-flex-end mt-auto">
-    <p>
-      Made with <strong><span style="color: red">♥</span></strong> by <strong>Kent Gruber</strong>
-    </p>
-  </div>
-</footer>
